46f84651d9cc1c703659100b91b9c3d3fd310fb8
kks <kwjkks@hanmail.net>
2022-07-06 21:23:42 +0900

    팩토리얼

--- 마지막 과제.c
+++ 마지막 과제.c
@@ -2,12 +2,13 @@
 #include <string.h>
 #include <stdbool.h>
 
+int main()
+
 {
-
-
-
-
-
-
-
+	int a, i = 1, fact = 1;
+	printf("Enter a number: ");
+	scanf_s("%d", &a);
+	for (i = 1; i <= a; i++)
+		fact = fact * i;
+	printf("The factorial of %d is %d. \n\n", a, fact);
 }
