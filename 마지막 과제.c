a68e5c1905b3cad877fa1c43f6599c332986311f
Hyeji Shin <s.hyeji20@gmail.com>
2022-07-07 08:43:27 +0900

    operation function added

--- 마지막 과제.c
+++ 마지막 과제.c
@@ -2,13 +2,52 @@
 #include <string.h>
 #include <stdbool.h>
 
+#include <iostream>
+using namespace std;
+// 편의를 위해 cout, cin 기능을 사용하는 것을 건의합니다. 
+
 int main()
 
 {
+	int n1, n2;
+	int result;
+	char math_sign;
+	cout << "대충 계산기 UI와 계산해보라는 소개 문구\n";
+	cin >> n1;
+	cin >> math_sign;
+	cin >> n2;
+
+	switch (math_sign) {
+	case '+': // n1 + n2 = result
+		result = n1 + n2;
+		break;
+	case '-': // n1 - n2 = result
+		result = n1 - n2;
+		break;
+	case '*': // n1 * n2 = result
+		result = n1 * n2;
+		break;
+	case '/': // n1/n2 = result
+		result = n1 / n2;
+		break;
+	case '^': // n1 ^ n2 = reslut (제곱)
+		result = pow(n1, n2);
+		break;
+	default:
+		cout << "잘못된 수학 부호입니다.";
+		break;
+
+	}
+	
+	cout << result;
+	
+
 	int a, i = 1, fact = 1;
 	printf("Enter a number: ");
 	scanf_s("%d", &a);
 	for (i = 1; i <= a; i++)
 		fact = fact * i;
 	printf("The factorial of %d is %d. \n\n", a, fact);
+
+
 }
